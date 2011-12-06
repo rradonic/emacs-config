4007595ffc99c28fad9ef25541d00604f7749be1
Ranko Radonić <ranko.radonic@gmx.com>
2011-12-06 19:24:06 +0100

    autoload major modes for different file types instead of loading them unconditionally at startup

--- .emacs
+++ .emacs
@@ -5,18 +5,9 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/yaml-mode"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/zenburn-emacs"))
 
+
 (require '_general)
 (require '_faces_default)
 ;; (require '_faces_zenburn)
 (require '_modes)
-
-(require 'clojure-mode)
-(require 'ediff)
-(require 'ibuffer)
-(require 'php-mode)
-(require 'tramp)
-(require 'vc)
-(require 'vc-svn)
-(require 'yaml-mode)
-
 (require '_file_types)
--- _file_types.el
+++ _file_types.el
@@ -1,12 +1,19 @@
+(autoload 'clojure-mode "clojure-mode")
+(autoload 'php-mode "php-mode")
+(autoload 'yaml-mode "yaml-mode")
+
 (add-to-list 'auto-mode-alist '("\\.rjs$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rxml$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.builder$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 
-(add-to-list 'auto-mode-alist '("\\.rhtml$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\.erb$" . html-mode))
-;; (add-to-list 'auto-mode-alist '("views/.*\\.php$" . html-mode))
+(add-to-list 'auto-mode-alist '("views/.*\\.php$" . html-mode))
+
+(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
