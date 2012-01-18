c89722a6ff863ba66d15f0405cd76e66daa2f059
Ranko Radonić <ranko.radonic@gmx.com>
2012-01-18 11:51:26 +0100

    registered some common extensions with their corresponding major modes
    i removed this when cleaning up for emacs 24, but it shouldn't have
    been removed

--- _file_types.el
+++ _file_types.el
@@ -2,4 +2,11 @@
 (autoload 'php-mode "php-mode")
 (autoload 'yaml-mode "yaml-mode")
 
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
 (provide '_file_types)
